5d03f1775351f89e659d8747987f4167f1269a53
snakee0101 <snakee0101@gmail.com>
2026-04-24 21:33:12 +0300

    improved site settings storage

--- database/connection_settings.php
+++ database/connection_settings.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
-    'host' => 'localhost',
-    'port' => 3306,
-    'username' => 'root',
-    'password' => 'root',
-    'database' => 'user_management',
+$local = [
+  'host' => 'localhost',
+  'port' => 3306,
+  'username' => 'root',
+  'password' => 'root',
+  'database' => 'user_management',
 ];
+
+$remote_hosting = [
+    'host' => 'sql211.infinityfree.com',
+    'port' => 3306,
+    'username' => 'if0_41747139',
+    'password' => '2001ab2211A',
+    'database' => 'if0_41747139_bootstrap_project',
+];
+
+return $local;
