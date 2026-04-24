6b76eba4a63667c5f16fa3e23517f6c616311def
snakee0101 <snakee0101@gmail.com>
2026-04-24 17:52:38 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User management system</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- JQuery -->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+
+    <!-- Custom Styling -->
+    <link rel="stylesheet" href="css/index.css">
+
+    <!-- Custom Script -->
+    <script src="js/index.js"></script>
+</head>
+<body>
+
+
+
+</body>
+</html>
